6bb480a830e91639d942e5ed750c2a12bf8ae922
David Bos <david.bos@outlook.com>
2024-11-27 20:57:55 +0100

    Patches + Banner systeem toegevoegd

--- Assets/Scripts/Icons.js
+++ Assets/Scripts/Icons.js
@@ -4,19 +4,27 @@ document.addEventListener('DOMContentLoaded', () => {
     function updateIcons() {
         const Arrows = document.querySelectorAll('.Icon-Arrow');
         const Githubs = document.querySelectorAll('.Icon-Github');
+        const CubeWarningIcon = document.querySelectorAll('.Icon-CubeWarningIcon');
     
         // Update alle pijlen
-        Arrows.forEach(arrow => {
-            arrow.src = prefersDarkScheme.matches 
+        Arrows.forEach(icon => {
+            icon.src = prefersDarkScheme.matches 
                 ? "/Assets/Icons/White/Arrow.svg" 
                 : "/Assets/Icons/Black/Arrow.svg";
         });
 
         // Update alle Github-iconen
-        Githubs.forEach(github => {
-            github.src = prefersDarkScheme.matches 
+        Githubs.forEach(icon => {
+            icon.src = prefersDarkScheme.matches 
                 ? "/Assets/Icons/White/Github.svg" 
                 : "/Assets/Icons/Black/Github.svg";
+        });
+
+        // Update alle CubeWarningIcon-iconen
+        CubeWarningIcon.forEach(icon => {
+            icon.src = prefersDarkScheme.matches 
+                ? "/Assets/Icons/White/CubeWarningIcon.svg" 
+                : "/Assets/Icons/Black/CubeWarningIcon.svg";
         });
     }
     
